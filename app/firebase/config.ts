0c600898dc3abd2c092add26d7daa04ed686c86d
sanirurajapaksha <sanirurajapaksha456@gmail.com>
2025-10-10 19:26:11 +0530

    Moved firebase credentials to .env file

--- app/firebase/config.ts
+++ app/firebase/config.ts
@@ -2,24 +2,14 @@ import { FirebaseApp, initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
-interface FirebaseConfig {
-    apiKey: string;
-    authDomain: string;
-    projectId: string;
-    storageBucket: string;
-    messagingSenderId: string;
-    appId: string;
-    measurementId: string;
-}
-
-const firebaseConfig: FirebaseConfig = {
-    apiKey: "AIzaSyBn798PJRXhv1AyI32sIlnqXKEj7j9lnTo",
-    authDomain: "crumbs-recipeapp.firebaseapp.com",
-    projectId: "crumbs-recipeapp",
-    storageBucket: "crumbs-recipeapp.firebasestorage.app",
-    messagingSenderId: "71791941949",
-    appId: "1:71791941949:web:80c7fe96235522869ecd54",
-    measurementId: "G-9N12SC8E82",
+const firebaseConfig = {
+    apiKey: process.env.EXPO_PUBLIC_apiKey,
+    authDomain: process.env.EXPO_PUBLIC_authDomain,
+    projectId: process.env.EXPO_PUBLIC_projectId,
+    storageBucket: process.env.EXPO_PUBLIC_storageBucket,
+    messagingSenderId: process.env.EXPO_PUBLIC_messagingSenderId,
+    appId: process.env.EXPO_PUBLIC_appId,
+    measurementId: process.env.EXPO_PUBLIC_measurementId,
 };
 
 // Initialize Firebase
